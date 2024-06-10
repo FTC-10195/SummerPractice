11c04bd176db5d70cc0ea3b47d2f50b80705c200
DoctorKrill08 <doctorkrill08@gmail.com>
2024-06-09 23:22:19 -0400

    removed repeats

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/MecanumDrive.java
@@ -38,10 +38,6 @@ public void move(double y, double x, double rx){
     double frontRightPower = ((y - x - rx) / denominator)*sensitivity;
     double backRightPower = ((y + x - rx) / denominator)*sensitivity;
 
-
-    frontLeftMotor.setPower(frontLeftPower);
-    backRightMotor.setPower(backRightPower);
-
     frontLeftMotor.setPower(frontLeftPower);
     backLeftMotor.setPower(backLeftPower);
     frontRightMotor.setPower(frontRightPower);
